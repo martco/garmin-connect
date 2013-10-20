c3c65f7dd22e5e40b68f9b7eb9ca00b429104f7a
Martin Cortez <martco@gmail.com>
2013-10-19 22:09:38 -0500

    adds functions and tests for timestamp, speed, and position

--- spec/TrackPointSpec.js
+++ spec/TrackPointSpec.js
@@ -1,5 +1,6 @@
 describe("GarminTrackPoint", function() {
   var trackPoint;
+  var garminDocument;
 
   beforeEach(function() {
     garminDocument = document.querySelector('iframe').contentDocument;
@@ -9,6 +10,31 @@ describe("GarminTrackPoint", function() {
   describe("getDistance()", function(){
     it("returns the distance in miles covered during the trackPoint", function() {
       expect(trackPoint.getDistance()).toBeCloseTo(0.0021, 2);
+    });
+  });
+
+  describe("getTimestamp()", function(){
+    it("returns the timestamp in the trackpoint", function() {
+      expect(trackPoint.getTimestamp()).toBe("2013-09-06T17:58:22.000Z");
+    });
+  });
+
+  describe("getPosition()", function(){
+    it("returns the latitude in the trackpoint if the trackpoint has position", function() {
+      expect(trackPoint.getPosition()).not.toBeDefined();
+
+      var garminTrackPoint = garminDocument.querySelectorAll('Trackpoint')[1122],
+          trackPointWithPosition = new GarminTrackPoint(garminTrackPoint, {}),
+          position = trackPointWithPosition.getPosition();
+
+      expect(position.latitude).toBe("41.92142511717975");
+      expect(position.longitude).toBe("-87.76414157822728");
+    });
+  });
+
+  describe("getSpeed()", function(){
+    it("returns miles per second", function() {
+      expect(trackPoint.getSpeed()).toBeCloseTo(0.0021, 2);
     });
   });
 
--- src/TrackPoint.js
+++ src/TrackPoint.js
@@ -4,6 +4,36 @@ function GarminTrackPoint(data, options){
   this.timeUnit = options.timeUnit || 'minute';
 };
 
+GarminTrackPoint.prototype.getTimestamp = function() {
+  t = this.data.querySelector('Time');
+  return t.textContent;
+}
+
+GarminTrackPoint.prototype.getSpeed = function() {
+  ext = this.data.querySelector('Extensions');
+  if(ext) {
+    tpx = this.data.querySelector('TPX');
+    if(tpx) {
+      speed = this.data.querySelector('Speed');
+      if(speed) { return this.formattedDistance(new Meters(speed.textContent)).toString(); }
+    } 
+  }
+}
+
+GarminTrackPoint.prototype.getPosition = function() {
+  var position = this.data.querySelector('Position');
+
+  if(position) {
+    var latitude = position.querySelector('LatitudeDegrees').textContent,
+        longitude = position.querySelector('LongitudeDegrees').textContent;
+
+    return {
+      latitude: latitude,
+      longitude: longitude
+    };
+  }
+}
+
 GarminTrackPoint.prototype.distanceSelector = GarminCourse.prototype.distanceSelector;
 GarminTrackPoint.prototype.formattedDistance = GarminCourse.prototype.formattedDistance;
 GarminTrackPoint.prototype.totalDistance = GarminCourse.prototype.totalDistance;
