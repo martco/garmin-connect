6d1a0081bdbf60574415249ce4f8bca556fedd17
Martin Cortez <martco@gmail.com>
2013-10-30 14:04:56 -0500

    Revert "adding rails"
    This reverts commit 9e6907b9b2687c5592c3d34ebe933cb5790f254c.
    
    Conflicts:
    	Gemfile
    	Gemfile.lock

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GarminConnect::Application.config.secret_token = 'e1a6eff96635534719cd210a5aae23be28a2c66eccc3d5072ef678dce7ce32fe4f6c6ec9c1810543eef31b7394b024bdab8a0abcb1ab6ccad5ad1341c31dc3a3'
